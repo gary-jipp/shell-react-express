3b81558f52dced95b37df229f96a2db8d0ba0538
gary <github@accu.ca>
2024-06-09 21:38:51 -0700

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import useData from './hooks/useData';
-import DataList from './components/Dataist';
+import DataList from './components/DataList';
 import './App.css';
 
 export default function App() {
